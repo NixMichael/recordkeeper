47713ce5d91ebd7668f1e9851cbccbe8fb22d7d5
Michael Nix <mpnix@protonmail.com>
2021-02-05 22:16:05 +0000

    Console log payload received by userReducers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,7 +1,7 @@
 export const logInReducer = (state = false, action) => {
+  console.log(`Accepted in reducer: ${action.payload}`)
   switch (action.type) {
     case 'LOGIN_USER':
-        console.log(`Accepted in reducer: ${action.payload}`)
         return action.payload
     case 'LOGOUT_USER':
       return false

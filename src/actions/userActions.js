afc5dd5ff06a6e59fb22e77f03aa9bdb6fce77b3
Michael Nix <mpnix@protonmail.com>
2021-02-05 22:42:26 +0000

    Handle incorrect user and password response

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,22 +1,26 @@
 import axios from 'axios'
 export const loginUser = (email, password) => async(dispatch) => {
 
-  console.log(email, password)
-  const accepted = await axios({
-    method: 'post',
-    url: 'https://morning-basin-38652.herokuapp.com/signin',
-    headers: {'Content-Type': 'application/json'},
-    data: {
-      email, password
-    }
-  })
+  try {
+    const accepted = await axios({
+      method: 'post',
+      url: 'https://morning-basin-38652.herokuapp.com/signin',
+      headers: {'Content-Type': 'application/json'},
+      data: {
+        email, password
+      }
+    })
 
-  console.log('Accepted:', accepted.data)
-
-  dispatch({
-    type: 'LOGIN_USER',
-    payload: accepted.data
-  })
+    dispatch({
+      type: 'LOGIN_USER',
+      payload: accepted.data
+    })
+  } catch (error) {
+    dispatch({
+      type: 'LOGIN_FAILED',
+      payload: error.message
+    })
+  }
 }
 
 export const logoutUser = () => {
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -3,6 +3,9 @@ export const logInReducer = (state = false, action) => {
     case 'LOGIN_USER':
         console.log(`Accepted in reducer: ${action.payload}`)
         return action.payload
+    case 'LOGIN_FAILED':
+        console.log(`incorrect credentials: ${action.payload}`)
+        return false
     case 'LOGOUT_USER':
       return false
     default:

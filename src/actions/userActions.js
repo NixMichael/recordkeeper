15919d004b5a97fc42b7954b43333e5f2eece0cf
Michael Nix <mpnix@protonmail.com>
2021-02-05 21:32:54 +0000

    Check username and password with database

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,12 +1,21 @@
-export const loginUser = (username, password) => {
+export const loginUser = (email, password) => async(dispatch) => {
+
+  const accepted = await axios({
+    method: 'post',
+    url: 'https://morning-basin-38652.herokuapp.com/signin',
+    headers: {'Content-Type': 'application/json'},
+    data: {
+      email, password
+    }
+  })
+
   return {
     type: 'LOGIN_USER',
-    payload: {username, password}
+    payload: accepted
   }
 }
 
 export const logoutUser = () => {
-  console.log('triggered')
   return {
     type: 'LOGOUT_USER'
   }
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,8 +1,7 @@
 export const logInReducer = (state = false, action) => {
-  // console.log('defined:', action.type)
   switch (action.type) {
     case 'LOGIN_USER':
-        return true
+        return action.payload
     case 'LOGOUT_USER':
       return false
     default:

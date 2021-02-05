9d8c776b7a9e67d5741a362618ef76ae232c67e0
Michael Nix <mpnix@protonmail.com>
2021-02-05 22:09:21 +0000

    Changed console log to display json result correctly

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -11,7 +11,7 @@ export const loginUser = (email, password) => async(dispatch) => {
     }
   })
 
-  console.log(`Accepted: ${accepted[0]}`)
+  console.log('Accepted:', accepted)
 
   return {
     type: 'LOGIN_USER',

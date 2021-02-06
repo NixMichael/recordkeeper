07705ed26ba6108c37f0a3f67fdb6eebe8357590
Michael Nix <mpnix@protonmail.com>
2021-02-06 11:43:17 +0000

    revert back

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -11,19 +11,6 @@ export const loginUser = (email, password) => async(dispatch) => {
       }
     })
 
-    // if (password === '') {
-    //   console.log('ask for a password')
-    //   const setPassword = prompt('Choose a password:')
-    //   await axios({
-    //     method: 'post',
-    //     url: 'https://morning-basin-38652.herokuapp.com/register',
-    //     headers: {'Content-Type': 'application/json'},
-    //     data: {
-    //       email, setPassword
-    //     }
-    //   })
-    // }
-
     dispatch({
       type: 'LOGIN_USER',
       payload: accepted.data
@@ -39,28 +26,5 @@ export const loginUser = (email, password) => async(dispatch) => {
 export const logoutUser = () => {
   return {
     type: 'LOGOUT_USER'
-  }
-}
-
-export const registerUser = (email, password) => async(dispatch) => {
-  try {
-    const result = axios({
-      method: 'post',
-      url: 'https://morning-basin-38652.herokuapp.com/register',
-      headers: {'Content-Type': 'application/json'},
-      data: {
-        email, password
-      }
-    })
-
-    dispatch({
-      type: 'PASSWORD_ACCEPTED',
-      payload: result.data
-    })
-  } catch (error) {
-    dispatch({
-      type: 'PASSWORD_DECLINED',
-      payload: error.message
-    })
   }
 }
